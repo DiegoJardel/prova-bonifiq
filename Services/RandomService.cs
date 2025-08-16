fd8d3224148026d204e13d322639e0861fb2e9fb
Diego Jardel Leite Nunes <31358763+DiegoJardel@users.noreply.github.com>
2025-08-16 15:20:19 -0300

    Update RandomService.cs
    Corrige RandomService: números aleatórios e únicos no banco

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -4,26 +4,33 @@ using ProvaPub.Repository;
 
 namespace ProvaPub.Services
 {
-	public class RandomService
-	{
-		int seed;
-        TestDbContext _ctx;
-		public RandomService()
+    public class RandomService
+    {
+        private readonly TestDbContext _ctx;
+        private readonly Random _random;
+
+        public RandomService()
         {
             var contextOptions = new DbContextOptionsBuilder<TestDbContext>()
-    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
-    .Options;
-            seed = Guid.NewGuid().GetHashCode();
+                .UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
+                .Options;
 
             _ctx = new TestDbContext(contextOptions);
+            _random = new Random();
         }
-        public async Task<int> GetRandom()
-		{
-            var number =  new Random(seed).Next(100);
-            _ctx.Numbers.Add(new RandomNumber() { Number = number });
-            _ctx.SaveChanges();
-			return number;
-		}
 
-	}
+        public async Task<int> GetRandom()
+        {
+            while (true)
+            {
+                var number = _random.Next(int.MaxValue);
+                if (!await _ctx.Numbers.AnyAsync(x => x.Number == number))
+                {
+                    _ctx.Numbers.Add(new RandomNumber { Number = number });
+                    await _ctx.SaveChangesAsync();
+                    return number;
+                }
+            }
+        }
+    }
 }
